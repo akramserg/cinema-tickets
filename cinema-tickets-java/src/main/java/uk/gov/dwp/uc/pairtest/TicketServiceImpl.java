cf21485829dac2366802b2f730537bf86eded29e
Akram Sergewa <akram.sergewa@gmail.com>
2026-05-02 11:27:48 +0100

    Added countTicketsByType. It loops through the ticket request and add the tickets per type

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -1,16 +1,39 @@
 package uk.gov.dwp.uc.pairtest;
 
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
+import java.util.EnumMap;
+import java.util.Map;
 
 public class TicketServiceImpl implements TicketService {
+
+    private final TicketPaymentService ticketPaymentService;
+    private final SeatReservationService seatReservationService;
+
+    public TicketServiceImpl(TicketPaymentService ticketPaymentService, SeatReservationService seatReservationService) {
+        this.ticketPaymentService = ticketPaymentService;
+        this.seatReservationService = seatReservationService;
+    }
+
     /**
      * Should only have private methods other than the one below.
      */
-
     @Override
     public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
 
+    }
+
+    /**
+     * Calculates ticket counts per ticket type.
+     */
+    private Map<TicketTypeRequest.Type, Integer> countTicketsByType(TicketTypeRequest[] ticketTypeRequests) {
+        Map<TicketTypeRequest.Type, Integer> counts = new EnumMap<>(TicketTypeRequest.Type.class);
+        for (TicketTypeRequest request : ticketTypeRequests) {
+            counts.merge(request.getTicketType(), request.getNoOfTickets(), Integer::sum);
+        }
+        return counts;
     }
 
 }
